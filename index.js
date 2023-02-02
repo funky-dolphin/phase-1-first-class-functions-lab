143a1020d44bdf6264aa7e3efa7d9fada637698b
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-02-02 15:41:25 -0500

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(names){
+    return names.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(names){
+    return names.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(number){
+    return function(fare){
+        return fare*number
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler =createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers=[],action){
+    if (action == returnFirstTwoDrivers)
+        return returnFirstTwoDrivers(drivers)
+    else (action == returnLastTwoDrivers)
+        return returnLastTwoDrivers(drivers)
+
+    }
